36c3a6c0d4828990c17b2848cbad760760b03c27
wangsn <123@qq.com>
2024-06-23 14:00:27 +0800

    自定义注解

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
@@ -2,10 +2,13 @@ package com.atguigu.gulimall.product.controller;
 
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
+import com.atguigu.common.valid.AddGroup;
+import com.atguigu.common.valid.UpdateGroup;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -52,7 +55,7 @@ public class BrandController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@Valid @RequestBody BrandEntity brand){
+    public R save(@Validated(AddGroup.class) @RequestBody BrandEntity brand){
 		brandService.save(brand);
 
         return R.ok();
@@ -62,7 +65,7 @@ public class BrandController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody BrandEntity brand){
+    public R update(@Validated(UpdateGroup.class) @RequestBody BrandEntity brand){
 		brandService.updateById(brand);
 
         return R.ok();
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
@@ -1,12 +1,14 @@
 package com.atguigu.gulimall.product.entity;
 
+import com.atguigu.common.valid.AddGroup;
+import com.atguigu.common.valid.ListValue;
+import com.atguigu.common.valid.UpdateGroup;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 
 /**
@@ -25,6 +27,8 @@ public class BrandEntity implements Serializable {
 	 * 品牌id
 	 */
 	@TableId
+	@NotNull(message = "编辑品牌id不能为空",groups = {UpdateGroup.class})
+	@Null(message = "新增品牌id必须为空",groups = {AddGroup.class})
 	private Long brandId;
 	/**
 	 * 品牌名
@@ -34,7 +38,8 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌logo地址
 	 */
-	@URL(message = "logo地址必须是url链接")
+	@URL(message = "logo地址必须是url链接",groups = {AddGroup.class,UpdateGroup.class})
+	@NotBlank(message = "logo地址不能为空",groups = {AddGroup.class,UpdateGroup.class})
 	private String logo;
 	/**
 	 * 介绍
@@ -43,10 +48,12 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 显示状态[0-不显示；1-显示]
 	 */
+	@ListValue(vals = {0,1},groups = {AddGroup.class})
 	private Integer showStatus;
 	/**
 	 * 检索首字母
 	 */
+	@Pattern(regexp = "^[a-zA-Z]$",message = "检索首字母必须是一个字母")
 	private String firstLetter;
 	/**
 	 * 排序
